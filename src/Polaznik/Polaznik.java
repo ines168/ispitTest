64e04591b7b4982cf99409919fc17e25c125b299
Ines <ines.cvek16@gmail.com>
2025-03-12 19:21:57 +0100

    polaznik: remove not used getters setters

--- src/Polaznik/Polaznik.java
+++ src/Polaznik/Polaznik.java
@@ -11,30 +11,6 @@ public class Polaznik implements Comparable<Polaznik> {
         this.prezime = prezime;
     }
 
-    public int getDob() {
-        return dob;
-    }
-
-    public void setDob(int dob) {
-        this.dob = dob;
-    }
-
-    public String getIme() {
-        return ime;
-    }
-
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
-    public String getPrezime() {
-        return prezime;
-    }
-
-    public void setPrezime(String prezime) {
-        this.prezime = prezime;
-    }
-
     @Override
     public String toString() {
         return "Polaznik{" +
